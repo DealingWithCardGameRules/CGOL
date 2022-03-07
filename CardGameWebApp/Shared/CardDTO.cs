f6fd88dd61e5ecff719ac57d077fa107b5e19bd1
Thomas Volden <tv@chargetime.eu>
2022-03-07 11:41:28 +0100

    Set up command handling in UI

---
+++ CardGameWebApp/Shared/CardDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CardGameWebApp.Shared
+{
+	public class CardDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Classification { get; set; }
+        public string Illustration { get; set; }
+        public IDictionary<string, string> Actions { get; set; }
+    }
+}
