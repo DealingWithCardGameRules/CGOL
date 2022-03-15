0a55ccbd08e8d6d67c0359c9309f6295ee3cf1a7
Thomas Volden <thvo@itu.dk>
2022-03-15 12:44:02 +0100

    Added command repository for storing commands.

---
+++ GameState/CommandRepository.cs
@@ -0,0 +1,17 @@
+﻿using dk.itu.game.msc.cgdl.CommandCentral;
+using System.Collections.Generic;
+
+namespace dk.itu.game.msc.cgdl.GameState
+{
+    public class CommandRepository
+    {
+        private List<ICommand> repository;
+
+        public CommandRepository()
+        {
+            repository = new List<ICommand>();
+        }
+
+
+    }
+}
