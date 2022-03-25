3f205b224fe1914bdb6a986d1a0bbb2d8ebcdbf4
Thomas Volden <thvo@itu.dk>
2022-03-25 14:52:47 +0100

    Added lex info for perm and inst keywords.

--- LanguageParser/Lexers/LexerFactory.cs
+++ LanguageParser/Lexers/LexerFactory.cs
@@ -28,6 +28,8 @@ namespace dk.itu.game.msc.cgdl.LanguageParser.Lexers
             yield return new RegexTokenDefinition(new TokenFactory(() => new SequenceTerminator()), @"^\r?\n");
             yield return new RegexTokenDefinition(new TokenFactory(() => new PlayKeyword()), @"^play\b");
             yield return new RegexTokenDefinition(new TokenFactory(() => new IfKeyword()), @"^if\b");
+            yield return new RegexTokenDefinition(new TokenFactory(() => new InstantaneousKeyword()), @"^(instantaneous|inst)\b");
+            yield return new RegexTokenDefinition(new TokenFactory(() => new PermanentKeyword()), @"^(permanent|perm)\b");
             yield return new RegexTokenDefinition(new TokenFactory(() => new ParenthesesStart()), @"^\(");
             yield return new RegexTokenDefinition(new TokenFactory(() => new ParenthesesEnd()), @"^\)");
             yield return new RegexTokenDefinition(new TokenFactory(() => new NumberLiteral()), @"^\d+");
