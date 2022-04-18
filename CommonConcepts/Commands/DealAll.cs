3e6b60e3aba73e3dd58248591a7c41d416f53562
Thomas Volden <thvo@itu.dk>
2022-04-18 23:42:34 +0200

    Added loop to deal multiple cards.

--- CommonConcepts/Commands/DealAll.cs
+++ CommonConcepts/Commands/DealAll.cs
@@ -11,7 +11,7 @@ namespace dk.itu.game.msc.cgdl.CommonConcepts.Commands
         public string Source { get; }
         public int Cards { get; }
 
-        public DealAll(string source, int cards)
+        public DealAll(string source, int cards = 1)
         {
             Instance = Guid.NewGuid();
             Source = source;
--- CommonConcepts/Commands/DealCard.cs
+++ CommonConcepts/Commands/DealCard.cs
@@ -9,14 +9,16 @@ namespace dk.itu.game.msc.cgdl.CommonConcepts.Commands
         public Guid ProcessId => new Guid("AAFE9EC9-EFA8-40DA-A37B-F3A856CFC5B0");
         [PlayCollection] public string Source { get; }
         public string? Destination { get; }
+        public int Cards { get; }
         public Guid Instance { get; }
 
         [Concept(Description = "Deal a card from one collection to an other collection. If the to parameter is not set, the destination is set to the current players hand.")]
-        public DealCard(string from, string to = null)
+        public DealCard(string from, string to = null, int cards = 1)
         {
             Instance = Guid.NewGuid();
             Source = from;
             Destination = to;
+            Cards = cards;
         }
     }
 }
