63cf0630c6ab2758b0d33a044dca401fcfb564b9
Thomas Volden <thvo@itu.dk>
2022-04-23 20:09:32 +0200

    Extended PickACard with a optional required parameter

--- CardGameWebApp/Shared/Inquiries/SelectCardInquiry.cs
+++ CardGameWebApp/Shared/Inquiries/SelectCardInquiry.cs
@@ -7,12 +7,14 @@ namespace CardGameWebApp.Shared.Inquiries
         public Guid CorrespondenceId { get; }
         public string Collection { get; }
         public string[]? RequiredTags { get; }
+        public bool Required { get; }
 
-        public SelectCardInquiry(Guid correspondenceId, string collection, string[]? requiredTags = null)
+        public SelectCardInquiry(Guid correspondenceId, string collection, string[]? requiredTags = null, bool required = true)
         {
             CorrespondenceId = correspondenceId;
             Collection = collection;
             RequiredTags = requiredTags;
+            Required = required;
         }
     }
 }
--- Representation/IUserEnquirier.cs
+++ Representation/IUserEnquirier.cs
@@ -4,7 +4,7 @@ namespace dk.itu.game.msc.cgdl.Representation
 {
     public interface IUserEnquirer
     {
-        Guid? SelectCard(int playerIndex, string collection, string[]? requiredTags);
+        Guid? SelectCard(int playerIndex, string collection, string[]? requiredTags, bool required);
         bool AskPlayer(int playerIndex, string message);
     }
 }
